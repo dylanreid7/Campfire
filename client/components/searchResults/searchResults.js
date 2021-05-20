fa390280e8f66fb6b4382f4bacd40c3271160f82
Dylan Reid <dylanreid7@gmail.com>
2021-05-20 07:15:25 -0700

    Committing before I try something that might break things.

--- client/components/searchResults/searchResults.js
+++ client/components/searchResults/searchResults.js
@@ -2,14 +2,24 @@ import React from 'react';
 import { makeStyles, Typography } from '@material-ui/core';
 import SearchItem from './searchItem.js';
 
+const useStyles = makeStyles({
+  spacing: {
+    marginTop: '20px'
+  }
+});
+
 export default function SearchResults({ facilities, campsites, checkInDate, checkOutDate }) {
   console.log('campsites: ', campsites);
+  const classes = useStyles();
 
   if (!facilities) {
     return (
-      <div id="search-picture">
-        <img src="https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"></img>
-      </div>
+      <>
+        <Typography variant="h2" className={classes.spacing}>Your next adventure is a click away...</Typography>
+        <div id="search-picture">
+          <img src="https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"></img>
+        </div>
+      </>
     )
   }
   if (campsites) {
